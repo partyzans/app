98735ff06508a4141a8c72ab9e9a74d3257470d4
Jolly Good <1671375+good-lly@users.noreply.github.com>
2019-11-23 17:30:00 +0100

    append working messaging between injected and background script

--- extension/background.js
+++ extension/background.js
@@ -1,12 +1,86 @@
 console.warn('hello!');
 const ENDPOINT = 'http://localhost:8000/upload/';
+const URL = 'isletnet.com';
 const pattern = 'https://isletnet.com/api/*';
+let tabId = 0;
 
 browser.browserAction.onClicked.addListener(function() {
-  chrome.tabs.create({ url: 'index.html' });
+  //browser.tabs.create({ url: 'https://isletnet.com/' }, onCreated);
+
+  chrome.tabs.query({}, function(tabs) {
+    var doFlag = true;
+    for (var i = tabs.length - 1; i >= 0; i--) {
+      if (url_domain(tabs[i].url) === URL) {
+        //your popup is alive
+        doFlag = false;
+        chrome.tabs.update(tabs[i].id, { active: true }); //focus it
+        tabId = tabs[i].id;
+        onExecuted();
+        break;
+      }
+    }
+    if (doFlag) {
+      //it didn't found anything, so create it
+      browser.tabs
+        .create({ url: 'https://isletnet.com/' })
+        .then(onCreated, onError);
+    }
+  });
 });
 
+function onCreated(newTab) {
+  // Use setTimeOut to give the loaded page some time to register the message listener
+  tabId = newTab.id;
+  setTimeout(onCreatedInner, 500);
+  function onCreatedInner() {
+    browser.tabs
+      .executeScript(tabId, {
+        file: 'inject.js',
+        allFrames: false,
+      })
+      .then(onExecuted, onError);
+  }
+}
+
+function onExecuted() {
+  chrome.tabs.sendMessage(tabId, { message: 'test' });
+}
+
+function onError(err) {
+  console.error('Eraaargh', err);
+}
+
+function url_domain(data) {
+  var a = document.createElement('a');
+  a.href = data;
+  return a.hostname;
+}
+
+function handleMessage(request, sender, sendResponse) {
+  console.log(`content script sent a message: ${request.content}`);
+  setTimeout(() => {
+    sendResponse({ response: 'async response from background script' });
+  }, 1000);
+  return true;
+}
+
+function sendMessage(content) {
+  let sending = chrome.tabs.sendMessage(tabId, {
+    content: content,
+  });
+  sending.then(handleResponse, handleError);
+}
+
+function handleResponse(message) {
+  console.log(`Background response:: ${message}`);
+}
+
+function handleError(error) {
+  console.log(`Background error:: ${error}`);
+}
+
 function logURL(requestDetails) {
+  sendMessage('loading');
   console.log('URL req: ' + requestDetails.url);
   if (requestDetails.requestBody.raw) {
     const data = requestDetails.requestBody.raw[0].bytes;
@@ -17,7 +91,6 @@ function logURL(requestDetails) {
     // console.warn('base64s', base64s);
     const pngs = base64s.map(base64 => atob(base64));
     // console.warn('str', pngs);
-
     sendPostReq(pngs);
   }
 }
@@ -39,6 +112,7 @@ function sendPostReq(data) {
   http.send(dataJSON);
 }
 
+browser.runtime.onMessage.addListener(handleMessage);
 browser.webRequest.onBeforeRequest.addListener(
   logURL,
   {
---
+++ extension/inject.js
@@ -0,0 +1,34 @@
+console.warn('INJECTED!');
+
+function handleResponse(message) {
+  console.log(`Response: ${message.response}`);
+}
+
+function handleError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function sendMessage(e) {
+  var sending = browser.runtime.sendMessage({
+    content: 'message from the content script',
+  });
+  sending.then(handleResponse, handleError);
+}
+
+function msgHandler(request, sender, sendResponse) {
+  console.warn(request.message);
+  sendResponse('ack');
+  return true;
+}
+
+function afterWindowLoaded() {
+  console.warn('loaded');
+}
+
+if (document.readyState !== 'complete') {
+  window.addEventListener('load', afterWindowLoaded);
+} else {
+  afterWindowLoaded();
+}
+
+chrome.runtime.onMessage.addListener(msgHandler);
